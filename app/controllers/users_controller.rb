29bce126f691a7bc2799d4b9366dfb6bf022584b
William Lowrimore <wlowrimore@gmail.com>
2022-09-16 17:15:34 -0500

    Finishes user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,25 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcom to the Sample App!"
+      redirect_to @user
+    else
+      render 'new', status: :unprocessable_entity
+    end  
+  end
+
+# -----------------------------------------------------
+# Private Parts
+# -----------------------------------------------------
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, 
+                                 :password_confirmation)
+  end
 end
